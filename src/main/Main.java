da0a5daf65f629cb6c9cab93dbb5d4de8e9acefd
vPerSos <vpsb888@gmail.com>
2024-04-30 11:57:14 +0200

    commit 1

---
+++ src/main/Main.java
@@ -0,0 +1,20 @@
+package main;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("");
+		String cadena = sc.next();
+		String letra;
+		for (int i = 0;i<cadena.length()-1;i++) {
+			letra = cadena.substring(i, i+1);
+			System.out.println(letr);
+		}
+		
+	}
+
+}
